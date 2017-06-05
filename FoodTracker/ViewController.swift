0a3dc45c3ebd15b6baee10cae1a8ab32dd8d8e37
Seunghun Lee <waydi1@gmail.com>
2017-06-05 09:52:02 +0900

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Seunghun Lee on 2017-06-05.
+//  Copyright © 2017 Seunghun Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
